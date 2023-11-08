d04fd4d7c875bd7b4d31581ed9f454489fcb4991
Amanda <amandaszirpolo@gmail.com>
2023-11-07 21:31:17 -0300

    edit biblioteca

--- funcoes_2.c
+++ funcoes_2.c
@@ -7,11 +7,12 @@
 
 typedef struct pagina {
     int tam_pagina; 
+    int *endereco_virtual; // endereco da pag na memoria virtual
     P *processo_associado; // processo a qual a pg esta associada
 } PG;
 
 typedef struct tabela_paginas {
-    PG *pagina;
+    //PG *pagina; -----> endereço virtual 
     int bit_p; // bit de presenca
     int bit_m; // bit de modificacao
     int end_quadro; // endereco da pagina na memoria principal
@@ -22,6 +23,7 @@ typedef struct processo {
     char *identificador; // nome do processo lido no arquivo
     char *estado_processo; // informa em qual estado o processo esta: novo, pronto, bloqueado, executando, finalizado
     int *tam_imagem; // tamanho do processo
+    int *endereco_fisico; // precisa disso aqui? uma vez q a propria variavel processo ja guarda seu endereço
     TP *tab_paginas; // tabela de paginas de um respectivo processo (qual estrutura de dados sera usada para a tabela de paginas?)
 
 } P;
--- processo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <locale.h>
-
-typedef struct processo {
-    char *estado_processo; // informa em qual estado o processo esta: novo, pronto, bloqueado, executando, finalizado
-    
-} P;
